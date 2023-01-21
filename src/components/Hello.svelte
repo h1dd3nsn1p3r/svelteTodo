731acc9a7025cd3a79cbf5e822c008b824a0d943
Anuj Subedi <struggleage@gmail.com>
2023-01-21 23:01:26 +0545

    Add: Initial commit.
    - Add: Initial commit.

---
+++ src/components/Hello.svelte
@@ -0,0 +1,5 @@
+<script></script>
+
+<div class="hello-component">
+	<h2>Hello Svelte!!!</h2>
+</div>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import App from './App.svelte';
+
+const app = new App({
+	target: document.getElementById('__svelte__'),
+	//props: {
+	//	name: 'world'
+	//}
+});
+
+export default app;
