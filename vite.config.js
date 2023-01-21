7820898999aa04dadc381bd96a38d549bf0ef538
Anuj Subedi <struggleage@gmail.com>
2023-01-21 23:13:50 +0545

    Tweak: vite config
    - Tweak: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+
+  resolve: {
+    alias: {
+
+      '@': resolve(__dirname, 'src/'),
+      '@assets': resolve(__dirname, 'src/assets/'),
+      '@component': resolve(__dirname, 'src/components/'),
+      '@utils': resolve(__dirname, 'src/utilities/'),
+    },
+  },
 })
